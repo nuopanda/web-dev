76d1fa3a5740320da92592f81e5fadbd48f577cc
Nuozhou Wang <wang.nu@husky.neu.edu>
2019-09-27 19:35:31 -0700

    first commit

---
+++ jQuery/index.js
@@ -0,0 +1,17 @@
+// $("h1").click(function(){
+//   $("h1").css("color", "red");
+// })
+//
+// $("button").click(function(){
+//   $("h1").css("color", "purple");
+// })
+//
+// $(document).keypress(function(){
+//   console.log(event.key);
+//   $("h1").text(event.key);
+// })
+//
+// $("h1").on("mouseover", function(){
+//   $("h1").css("color", "purple");
+// })
+$("h1").before("<button>new</button>");
